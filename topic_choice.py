f9c360dd25db3df45eca78326d1234f323eb7c58
muteki938 <muteki938@gmail.com>
2026-04-02 13:19:23 -0400

    added topic_choice class and math button

---
+++ topic_choice.py
@@ -0,0 +1,34 @@
+import arcade
+import arcade.gui
+from constants import *
+
+class Topic_choice(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+        math_button = arcade.gui.UIFlatButton(text="Mathematics", width=200, height=60)
+        math_button.center_x = 120
+        math_button.center_y = 330
+        math_button.on_click = self.change_to_game_math
+        self.manager.add(math_button)
+    
+
+    
+
+    def on_draw(self):
+        self.manager.draw()
+    
+
+    def change_to_game_math(self, event):
+        print("changed to game math")
+
+
+
+
+if __name__ == "__main__":
+    game = Topic_choice()
+    arcade.run()
+
+
